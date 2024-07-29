9d5d7aa9e60288c1037b84d362b0af423d5047ee
DanielKL <80163243+Redocer-kl@users.noreply.github.com>
2024-07-29 22:50:56 +0700

    Edit

--- module2_3.py
+++ module2_3.py
@@ -1,6 +1,8 @@
 my_list = list(map(int, input("Введите числа в списке через пробел: ").split()))
 i = 0
-while my_list[i] >= 0 and i < len(my_list):
+while i < len(my_list):
+    if my_list[i] < 0 :
+        break
     if my_list[i] == 0:
         continue
     print(my_list[i])
