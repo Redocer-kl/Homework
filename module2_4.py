905193b6243c58ff05bbecf51cf66a49ee0184c5
DanielKL <80163243+Redocer-kl@users.noreply.github.com>
2024-07-29 16:34:23 +0700

    Домашняя работа по уроку "Цикл for. Элементы списка. Полезные функции в цикле"

---
+++ module2_4.py
@@ -0,0 +1,17 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in numbers:
+    f = True
+    d = 2
+    while d * d <= i:
+        if i % d == 0:
+            not_primes.append(i)
+            f = False
+            break
+        d+= 1
+    if f:
+        primes.append(i)
+
+print("Простые числа: ", *primes)
+print("Сложносоставные числа: ", *not_primes)
