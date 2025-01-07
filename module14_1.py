5aab558de4b412b4b7b53b45a5d25b957d2bc714
DanielKL <fwydegg@yandex.ru>
2025-01-07 16:14:29 +0700

    2024/01/29 00:00|Домашнее задание по теме "Создание БД, добавление, выбор и удаление элементов."

---
+++ module14_1.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+# Создаём базу данных и подключаемся к ней
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+# Создание таблицы Users, если она ещё не создана
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER,
+    balance INTEGER NOT NULL
+)
+''')
+
+for i in range(1, 11):
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)', (f'user{i}', f'example{i}@gmail.com', i * 10, 1000))
+
+cursor.execute('UPDATE Users SET balance = 500 WHERE id % 2 = 1')
+
+cursor.execute('DELETE FROM Users WHERE id % 3 = 1')
+
+# Смотрим все записи, где возраст не равен 60
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != 60')
+rows = cursor.fetchall()
+
+for row in rows:
+    print(f'Имя: {row[0]} | Почта: {row[1]} | Возраст: {row[2]} | Баланс: {row[3]}')
+
+connection.commit()
+connection.close()
