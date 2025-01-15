5a506504f94220f219234cd086a5af573634b13f
DanielKL <fwydegg@yandex.ru>
2025-01-15 19:38:15 +0700

    2024/02/02 00:00|Домашнее задание по теме "Написание примитивной ORM"

--- crud_functions.py
+++ crud_functions.py
@@ -11,6 +11,15 @@ def initiate_db():
     price INTEGER
     )
     ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Users(
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        balance INTEGER NOT NULL
+        )
+        ''')
     connection.commit()
 
 def get_all_products():
@@ -20,7 +29,29 @@ def get_all_products():
     return cursor.fetchall()
 
 
+def add_user(username, email, age):
+    connection = sqlite3.connect("not_telegram.db")
+    if not is_included(username):
+        cursor.execute('''
+        INSERT INTO Users (username, email, age, balance)
+        VALUES (?, ?, ?, ?)
+        ''', (username, email, age, 1000))
 
+    connection.commit()
+    connection.close()
+
+
+def is_included(username):
+    connection = sqlite3.connect("not_telegram.db")
+
+    cursor.execute('''
+    SELECT EXISTS(SELECT 1 FROM Users WHERE username=?)
+    ''', (username,))
+
+    result = cursor.fetchone()[0]
+    connection.close()
+
+    return bool(result)
 
 initiate_db()
 
